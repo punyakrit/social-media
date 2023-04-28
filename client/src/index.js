f9f275454c78e896fdc2355e75bd7b89a204c927
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2023-04-28 17:43:29 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import {
+import { 
   persistStore,
   persistReducer,
   FLUSH,
@@ -17,6 +17,9 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
